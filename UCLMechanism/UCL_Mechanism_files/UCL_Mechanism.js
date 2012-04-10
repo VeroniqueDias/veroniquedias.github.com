b948a3943657cda5e44556b31a801123685b67a9
Véronique Dias <veronique.dias@uclouvain.be>
2012-04-10 11:27:38 +0200

    last version

--- UCLMechanism/UCL_Mechanism_files/UCL_Mechanism.js
+++ UCLMechanism/UCL_Mechanism_files/UCL_Mechanism.js
@@ -1,9 +1,11 @@
-// Created by iWeb 3.0.4 local-build-20120329
+// Created by iWeb 3.0.4 local-build-20120410
 
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-5,5,10,62),url:'UCL_Mechanism_files/stroke.png'},{rect:new IWRect(-5,-5,10,10),url:'UCL_Mechanism_files/stroke_1.png'},{rect:new IWRect(5,-5,39,10),url:'UCL_Mechanism_files/stroke_2.png'},{rect:new IWRect(44,-5,10,10),url:'UCL_Mechanism_files/stroke_3.png'},{rect:new IWRect(44,5,10,62),url:'UCL_Mechanism_files/stroke_4.png'},{rect:new IWRect(44,67,10,10),url:'UCL_Mechanism_files/stroke_5.png'},{rect:new IWRect(5,67,39,10),url:'UCL_Mechanism_files/stroke_6.png'},{rect:new IWRect(-5,67,10,10),url:'UCL_Mechanism_files/stroke_7.png'}],new IWSize(49,72))});registry.applyEffects();}
+function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('UCL_Mechanism_files/UCL_MechanismMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');adjustLineHeightIfTooBig('id10');adjustFontSizeIfTooBig('id10');adjustLineHeightIfTooBig('id11');adjustFontSizeIfTooBig('id11');adjustLineHeightIfTooBig('id12');adjustFontSizeIfTooBig('id12');adjustLineHeightIfTooBig('id13');adjustFontSizeIfTooBig('id13');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');adjustLineHeightIfTooBig('id10');adjustFontSizeIfTooBig('id10');adjustLineHeightIfTooBig('id11');adjustFontSizeIfTooBig('id11');adjustLineHeightIfTooBig('id12');adjustFontSizeIfTooBig('id12');adjustLineHeightIfTooBig('id13');adjustFontSizeIfTooBig('id13');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
